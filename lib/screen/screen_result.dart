d977e51ccd6e892ac3c344ed8f0eaf3db273f92f
metacogpe <metacog@kakao.com>
2022-11-13 21:58:09 +0900

    결과와 홈으로 돌아가기 버튼

--- lib/screen/screen_result.dart
+++ lib/screen/screen_result.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/model/model_quiz.dart';
+import 'package:quiz_app/screen/screen_home.dart';
 
 class ResultScreen extends StatelessWidget {
   List<int> answers;
@@ -9,9 +10,7 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery
-        .of(context)
-        .size; // 반응형 위한 사이즈 가져오기
+    Size screenSize = MediaQuery.of(context).size; // 반응형 위한 사이즈 가져오기
     double width = screenSize.width;
     double height = screenSize.height;
 
@@ -38,11 +37,89 @@ class ResultScreen extends StatelessWidget {
             width: width * 0.85,
             height: height * 0.5,
             child: Column(
-              children: <Widget>[],
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: width * 0.048),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.deepPurple),
+                    color: Colors.white,
+                  ),
+                  width: width * 0.73,
+                  height: height * 0.25,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(
+                            0, width * 0.048, 0, width * 0.012),
+                        child: Text(
+                          '완료하셨습니다.',
+                          style: TextStyle(
+                            fontSize: width * 0.055,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '당신의 스코어',
+                        style: TextStyle(
+                          fontSize: width * 0.048,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Text(
+                        score.toString() + '/' + quizs.length.toString(),
+                        style: TextStyle(
+                          fontSize: width * 0.21,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(width * 0.012),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: width * 0.048),
+                  child: ButtonTheme(
+                    minWidth: width * 0.73,
+                    height: height * 0.05,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return HomeScreen();
+                        }));
+                      },
+                      child: Text(
+                        '홈으로 돌아가기',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
-     ),
+      ),
     );
   }
 }
