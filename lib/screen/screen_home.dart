2a1eeedfa151360ed92cb75b60724591fc183e82
metacogpe <metacog@kakao.com>
2022-11-13 12:26:38 +0900

    퀴즈 더미 데이터 생성: 퀴즈시작 버튼 시 동작 방식

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../model/model_quiz.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Quiz> quizs = [
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
+    Quiz.fromMap({
+      'title': 'test',
+      'candidates': ['a', 'b', 'c', 'd'],
+      'answer': 0
+    }),
+  ];
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size; // 반응형 위한 사이즈 가져오기
