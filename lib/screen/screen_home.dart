8227baeea4503103d8d75f263164d6137867889d
metacogpe <metacog@kakao.com>
2022-11-13 10:30:31 +0900

    _buildStep : 설명문 함수 별도 생성

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -8,47 +8,69 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;  // 반응형 위한 사이즈 가져오기
+    Size screenSize = MediaQuery.of(context).size; // 반응형 위한 사이즈 가져오기
     double width = screenSize.width;
     double height = screenSize.height;
-    
-    return SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Quiz App'),
-            backgroundColor: Colors.deepPurple,
-            leading: Container(),  // AppBar에서 페이지 이동시 뒤로가기 버튼 지우기 효과
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                child: Image.asset(
-                  'images/quiz.jpg', 
-                  width: width * 0.8,
-                ),
-              ),
-              Padding(padding: EdgeInsets.all(width * 0.024),
-              ),
-              Text(
-                'Flutter Quiz App',
-                style: TextStyle(
-                  fontSize: width * 0.065,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Info About Quiz\n 퀴즈 풀기를 눌러 주세요.',
-                textAlign: TextAlign.center,
-              ),
-              Padding(
-                  padding: EdgeInsets.all(width * 0.048),
-              ),
 
-            ],
-          ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+          backgroundColor: Colors.deepPurple,
+          leading: Container(), // AppBar에서 페이지 이동시 뒤로가기 버튼 지우기 효과
         ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Image.asset(
+                'images/quiz.jpg',
+                width: width * 0.8,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.024),
+            ),
+            Text(
+              'Flutter Quiz App',
+              style: TextStyle(
+                fontSize: width * 0.065,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Info About Quiz\n 퀴즈 풀기를 눌러 주세요.',
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            _buildStep(width, '1. 랜덤 퀴즈'),
+          ],
+        ),
+      ),
     );
   }
+}
+
+// 안내문을 별도의 함수로 생성
+Widget _buildStep(double width, String title) {
+  return Container(
+    padding: EdgeInsets.fromLTRB(
+        width * 0.048, width * 0.024, width * 0.048, width * 0.024),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Icon(
+          Icons.check_box,
+          size: width * 0.04,
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: width * 0.024),
+        ),
+        Text(title),
+      ],
+    ),
+  );
 }
