1f59a4a970c0268a93f5c4f50bb4e97052af6376
metacogpe <metacog@kakao.com>
2022-11-13 13:53:32 +0900

    퀴즈 박스 : 퀴즈 번호와 퀴즈 타이틀

--- lib/screen/screen_quiz.dart
+++ lib/screen/screen_quiz.dart
@@ -19,45 +19,61 @@ class _QuizScreenState extends State<QuizScreen> {
   int _currentIndex = 0; // 현재 보고 있는 퀴즈
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery
-        .of(context)
-        .size; // 반응형 위한 사이즈 가져오기
+    Size screenSize = MediaQuery.of(context).size; // 반응형 위한 사이즈 가져오기
     double width = screenSize.width;
     double height = screenSize.height;
     return SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.deepPurple,
-          body: Center(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(color: Colors.deepPurple),
-              ),
-              width: width * 0.85,
-              height: height * 0.5,
-              child: Swiper(
-                physics: NeverScrollableScrollPhysics(),
-                loop: false,
-                itemCount: widget.quizs.length,
-                itemBuilder: (BuildContext context, int index){
-                  return _buildQuizCard(widget.quizs[index], width, height);
-                },
-              ),
+      child: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.deepPurple),
+            ),
+            width: width * 0.85,
+            height: height * 0.5,
+            child: Swiper(
+              physics: NeverScrollableScrollPhysics(),
+              loop: false,
+              itemCount: widget.quizs.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _buildQuizCard(widget.quizs[index], width, height);
+              },
             ),
           ),
         ),
+      ),
     );
   }
 
   Widget _buildQuizCard(Quiz quiz, double width, double height) {
     return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(color: Colors.white)
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Colors.white)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(0, width * 0.024, 0, width * 0.024),
+            child: Text(
+              'Q' + (_currentIndex + 1).toString() + '.',
+              style: TextStyle(
+                fontSize: width * 0.06,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            width: width * 0.8,
+            padding: EdgeInsets.only(top: width * 0.012),
+            child: Text(
+              quiz.title,
+            ),
+          )
+        ],
       ),
     );
   }
 }
-
-
-
